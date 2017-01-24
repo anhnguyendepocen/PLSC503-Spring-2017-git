ac5c04f0121ef948e602f2bbf4b40af90d7b9a25
Christopher Zorn <zorn@psu.edu>
2017-01-24 16:08:05 -0500

    Code

---
+++ Code/PLSC503-2016-DayFiveCode.R
@@ -0,0 +1,69 @@
+######################################
+# Code for PLSC 503 Day Five
+# (Spring 2017)
+#
+# File created 1/24/2017
+#
+# File last modified 1/24/2017
+######################################
+# setwd() first:
+#
+# setwd("~/PLSC 503/Notes") # or whatever...
+
+seed <- 7222009
+set.seed(seed)
+
+X<-rnorm(250)
+Y1<-5+2*X+rnorm(250,mean=0,sd=sqrt(0.2))
+Y2<-5+2*X+rnorm(250,mean=0,sd=sqrt(20))
+fit<-lm(Y1~X)
+summary(fit)
+
+pdf("TightLine-R.pdf",5,5)
+plot(X,Y1,pch=20,xlab="X",ylab="Y",
+     xlim=c(-2.5,2.5),ylim=c(-10,15))
+abline(fit,lwd=3)
+text(-1.5,12,labels="R-squared = 0.95")
+dev.off()
+
+fit2<-lm(Y2~X)
+summary(fit2)
+
+pdf("ScatteredLine-R.pdf",5,5)
+plot(X,Y2,pch=20,xlab="X",ylab="Y",
+     xlim=c(-2.5,2.5),ylim=c(-10,15))
+abline(fit2,lwd=3)
+text(-1.5,12,labels="R-squared = 0.20")
+dev.off()
+
+# Three-observation regression:
+
+X<-c(1,2,3)
+Y<-c(1,1,7)
+WSR<-lm(Y~X)
+summary(WSR)
+
+# R^2 = 0 plots:
+
+seed<-7222009
+set.seed(seed)
+
+pdf("RSqZero.pdf",7,6)
+par(mfrow=c(2,2))
+par(mar=c(4,4,2,2))
+X<-(runif(100))*10
+Yzero<-(runif(100))*10
+Yquad<-30-((X-5)^2)+(2*runif(100))
+Ystep<-ifelse(abs(X-5)>2.5,5+runif(100),1+runif(100))
+Ytype<-rep(0:1,50)
+Yvar<-ifelse(Ytype==1,X+(2*runif(50)),10-X+2*runif(50))
+plot(Yzero~X,xlab="X", ylab="Y",pch=20)
+abline(lm(Yzero~X),lwd=3)
+plot(Yquad~X,xlab="X", ylab="Y",pch=20)
+abline(lm(Yquad~X),lwd=3)
+plot(Ystep~X,xlab="X", ylab="Y",pch=20)
+abline(lm(Ystep~X),lwd=3)
+plot(Yvar[Ytype==0]~X[Ytype==0],xlab="X", ylab="Y",pch=20)
+points(Yvar[Ytype==1]~X[Ytype==1],pch="o")
+abline(lm(Yvar~X),lwd=3)
+dev.off()
