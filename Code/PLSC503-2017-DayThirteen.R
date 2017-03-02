af669917e62bdcb6a12aae6c42036651a7b35ab2
Christopher Zorn <zorn@psu.edu>
2017-03-02 10:27:13 -0500

    Code

---
+++ Code/PLSC503-2017-DayThirteen.R
@@ -0,0 +1,38 @@
+##################################################
+# PLSC 503 -- Spring 2017: Code for Day Thirteen.
+##################################################
+
+# Africa examples:
+
+library(RCurl)
+library(stargazer)
+temp<-getURL("https://raw.githubusercontent.com/PrisonRodeo/PLSC503-Spring-2017-git/master/Data/africa2001.csv")
+Africa<-read.csv(text=temp, header=TRUE)
+rm(temp)
+
+# Perfect multicollinearity:
+
+Africa$newgdp<-(Africa$gdppppd-mean(Africa$gdppppd))*1000
+
+fit<-with(Africa, lm(adrate~gdppppd+newgdp+healthexp+subsaharan+
+                       muslperc+literacy))
+summary(fit)
+
+# N = K
+
+smallAfrica<-subset(Africa,subsaharan=="Not Sub-Saharan")
+fit2<-with(smallAfrica,lm(adrate~gdppppd+healthexp+muslperc+
+                            literacy+war))
+summary(fit2)
+
+# MCLin:
+
+with(Africa, table(internalwar,intensity))
+
+HIV1<-with(Africa, lm(adrate~internalwar))
+HIV2<-with(Africa, lm(adrate~intensity))
+HIV3<-with(Africa, lm(adrate~internalwar+intensity))
+
+stargazer(HIV1,HIV2,HIV3)
+
+
